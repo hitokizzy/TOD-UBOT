58b063bf4f3b95b195d7b6751ee35143fe0b9202
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-04-01 01:56:59 +0700

    Update session.py

--- ibels/core/session.py
+++ ibels/core/session.py
@@ -5,6 +5,7 @@ from ..Config import Config
 from .client import ibelubotClient
 from pytgcalls import PyTgCalls
 
+
 __version__ = "2.0.0"
 
 loop = None
@@ -19,11 +20,13 @@ try:
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
         connection=ConnectionTcpAbridged,
         auto_reconnect=True,
         connection_retries=None,
     )
-    call_py = PyTgCalls(ibel)
+    call_py = PyTgCalls(Config.STRING_SESSION, Config.APP_ID, Config.API_HASH)
 except Exception as e:
     print(f"STRING_SESSION - {e}")
     sys.exit()
