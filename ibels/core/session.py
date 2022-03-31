b8a0b13db6ef61c1464e4ff17d96180bf792c6a8
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-03-31 21:26:29 +0700

    Update session.py

--- ibels/core/session.py
+++ ibels/core/session.py
@@ -3,7 +3,7 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 from ..Config import Config
 from .client import ibelubotClient
-
+from pytgcalls import PyTgCalls
 
 __version__ = "2.0.0"
 
