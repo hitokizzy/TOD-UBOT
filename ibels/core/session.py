69531c19adc930c6c165bfc5b42c505b3107fd70
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-04-01 01:33:12 +0700

    Update session.py

--- ibels/core/session.py
+++ ibels/core/session.py
@@ -19,8 +19,6 @@ try:
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        loop=loop,
-        app_version=__version__,
         connection=ConnectionTcpAbridged,
         auto_reconnect=True,
         connection_retries=None,
