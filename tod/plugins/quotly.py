d2ec333a76a6706e03a583d467db81934aa76eeb
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-04-04 18:25:06 +0700

    Update quotly.py

--- tod/plugins/quotly.py
+++ tod/plugins/quotly.py
@@ -59,7 +59,7 @@ async def q_pic(event):  # sourcery no-metrics
         return await edit_delete(
             event, "__Provide input along with cmd or reply to text message.__"
         )
-    sadevent = await edit_or_reply(event, "__Making Quote pic....__")
+    catevent = await edit_or_reply(event, "__Making Quote pic....__")
     mediatype = media_type(reply)
     if (
         (not reply)
@@ -67,7 +67,7 @@ async def q_pic(event):  # sourcery no-metrics
         or (mediatype not in ["Photo", "Sticker"])
         or (
             mediatype == "Sticker"
-            and reply.document.mime_type == "applisadion/i-tgsticker"
+            and reply.document.mime_type == "application/i-tgsticker"
         )
     ):
         user = reply.sender_id if reply else event.client.uid
@@ -123,87 +123,17 @@ async def q_pic(event):  # sourcery no-metrics
         )
     output = io.BytesIO()
     if sticker:
-        output.name = "TODUBOT.Webp"
+        output.name = "todqoutly.Webp"
         img.save(output, "webp")
     else:
-        output.name = "TODUBOT.png"
+        output.name = "todqoutly.png"
         img.save(output, "PNG")
     output.seek(0)
     await event.client.send_file(event.chat_id, output, reply_to=reply_to)
-    await sadevent.delete()
+    await catevent.delete()
     for i in [pfp]:
         if os.path.lexists(i):
             os.remove(i)
-
-
-@tod.tod_cmd(
-    pattern="q(?:\s|$)([\s\S]*)",
-    command=("q", plugin_category),
-    info={
-        "header": "Makes your message as sticker quote.",
-        "usage": "{tr}q",
-    },
-)
-async def quott_(event):
-    match = event.pattern_match.group(1).strip()
-    if not event.is_reply:
-        return await event.eor("`Reply to Message..`")
-    msg = await event.eor("Processing...")
-    reply = await event.get_reply_message()
-    replied_to, reply_ = None, None
-    if match:
-        spli_ = match.split(maxsplit=1)
-        if (spli_[0] in ["r", "reply"]) or (
-            spli_[0].isdigit() and int(spli_[0]) in range(1, 21)
-        ):
-            if spli_[0].isdigit():
-                if not event.client._bot:
-                    reply_ = await event.client.get_messages(
-                        event.chat_id,
-                        min_id=event.reply_to_msg_id - 1,
-                        reverse=True,
-                        limit=int(spli_[0]),
-                    )
-                else:
-                    id_ = reply.id
-                    reply_ = []
-                    for msg_ in range(id_, id_ + int(spli_[0])):
-                        msh = await event.client.get_messages(event.chat_id, ids=msg_)
-                        if msh:
-                            reply_.append(msh)
-            else:
-                replied_to = await reply.get_reply_message()
-            try:
-                match = spli_[1]
-            except IndexError:
-                match = None
-    user = None
-    if not reply_:
-        reply_ = reply
-    if match:
-        match = match.split(maxsplit=1)
-    if match:
-        if match[0].startswith("@") or match[0].isdigit():
-            try:
-                match_ = await event.client.parse_id(match[0])
-                user = await event.client.get_entity(match_)
-            except ValueError:
-                pass
-            match = match[1] if len(match) == 2 else None
-        else:
-            match = match[0]
-    if match == "random":
-        match = choice(all_col)
-    try:
-        file = await quotly.create_quotly(
-            reply_, bg=match, reply=replied_to, sender=user
-        )
-    except Exception as er:
-        return await msg.edit(str(er))
-    message = await reply.reply("Quotly by TOD-UBOT", file=file)
-    os.remove(file)
-    await msg.delete()
-    return message
 
 
 @tod.tod_cmd(
