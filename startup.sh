7f89a0a31c96cbdd8830c640ec60c2e7015ce890
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-04-04 23:38:49 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,10 @@ then
 	echo "Add SESSION_NAME or STRING_SESSION first!"
 elif [[ -z "$STRING_SESSION" ]]
 then
-	python3 -m tod
+	python3 -m main.py
 elif [[ -z "$SESSION_NAME" ]]
 then
-	python -m main.py
+	python -m tod
 else
 	python3 -m tod & python3 -m main.py
 	exit 1
