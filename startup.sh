b9a5f16309c7e00d163dac642b8f30d71b3bdc74
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-04-05 23:30:12 +0700

    ini ibu budi

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
+# NOT FIXED YET #
 if [[ -z "$STRING_SESSION" && -z "$SESSION_NAME" ]]
 then
 	echo "Add SESSION_NAME or STRING_SESSION first!"
@@ -8,6 +9,6 @@ elif [[ -z "$SESSION_NAME" ]]
 then
 	python -m tod
 else
-	python3 -m tod & python3 main.py
+	python3 main.py & python3 -m tod
 	exit 1
 fi
